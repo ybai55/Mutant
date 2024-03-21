f92340936cbe6f02363957c79815690831c244bb
ybai55 <ybai24@hawk.iit.edu>
2024-03-21 11:00:11 +0800

    WIP end-to-end working

---
+++ mutant-server/mutant_server/test/test_client.py
@@ -0,0 +1,16 @@
+from mutant_client import Mutant
+import pytest
+import time
+from httpx import AsyncClient
+from ..api import app
+
+
+@pytest.fixture
+def anyio_backend():
+    return 'asyncio'
+
+
+def test_init():
+    mutant = Mutant()
+    assert mutant._api_url == "http://localhost:8000/api/v1"
+
