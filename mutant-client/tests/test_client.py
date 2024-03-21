e3d54224804371d5276f08679f089f614d6355f8
ybai55 <ybai24@hawk.iit.edu>
2024-03-21 16:41:56 +0800

    fix several things. try changes in settings.

--- mutant-client/tests/test_client.py
+++ mutant-client/tests/test_client.py
@@ -1,6 +1,13 @@
-import mutant_client
+from mutant_client import Mutant
 import pytest
+import time
+from httpx import AsyncClient
 
+
+@pytest.fixture
+def anyio_backend():
+    return 'asyncio'
 
 def test_init():
-    assert mutant_client.init() == True
+    mutant = Mutant()
+    assert mutant._api_url == 'http://localhost:8000/api/v1'
