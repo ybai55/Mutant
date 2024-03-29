483659a0c2a3d01d0f08e4800c8e478082a47bf8
ybai55 <ybai24@hawk.iit.edu>
2024-03-29 08:31:23 +0800

    shell files for packaging and setup

---
+++ bin/setup_linux.sh
@@ -0,0 +1,40 @@
+# install pip
+apt install python3-pip
+
+# install docker
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+pip3 install docker-compose
+
+
+# get the code
+git clone https://github.com/ybai55/Mutant.git
+
+#checkout the right branch
+cd mutant
+git checkout jeff/clickhouse
+
+# run docker
+cd mutant-server
+docker-compose up -d --build
+
+# install mutant-client
+cd ../mutant-client
+pip3 install --upgrade pip # you have to do this or it will use UNKNOWN as the package name
+pip3 install .
