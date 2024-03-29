664631dff951b91a52ecda16767f0f55994dc98d
ybai55 <ybai24@hawk.iit.edu>
2024-03-29 08:34:07 +0800

    default scope and small test script

---
+++ bin/test.py
@@ -0,0 +1,22 @@
+from mutant_client import Mutant
+
+mutant =Mutant()
+mutant.set_space_key('sample_space')
+print("Getting heartbeat to verify the server is up")
+print(mutant.heartbeat())
+
+print("Logging embeddings into the database")
+mutant.log(
+    [[1,2,3,4,5], [5,4,3,2,1], [10,9,8,7,6]],
+    ["/images/1", "/images/2", "/images/3"],
+    ["training", "training", "training"],
+    ['spoon', 'knife', 'fork']
+)
+
+print("Generating the index")
+print(mutant.process())
+
+print("Running a nearest neighbor search")
+print(mutant.get_nearest_neighbors([1, 2, 3, 4, 5], 1))
+
+print("Success Everything worked!")
