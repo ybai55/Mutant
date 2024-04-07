b4ea569d57b67319817f7a2f0f77f690678c61c5
ybai55 <ybai24@hawk.iit.edu>
2024-04-07 10:14:07 +0800

    fix bin/test.py

--- bin/test.py
+++ bin/test.py
@@ -1,28 +1,37 @@
 # Sanity check script to ensure that the Mutant client can connect
 # and is capable of recieving data.
 
-from mutant_client import Mutant
+import mutant
+from mutant.config import Settings
 
-mutant =Mutant()
-mutant.set_model_space('sample_space')
+# run in in-memory mode
+mutant_api = mutant.get_api()
+
+# uncomment to run in client-server mode
+# chroma_api = chroma.get_api(Settings(chroma_api_impl="rest",
+#                               chroma_server_host="localhost",
+#                               chroma_server_http_port="8000") )
+
+mutant_api.set_model_space("sample_space")
 print("Getting heartbeat to verify the server is up")
-print(mutant.heartbeat())
+print(mutant_api.heartbeat())
 
 print("Logging embeddings into the database")
-mutant.add(
-    [[1,2,3,4,5], [5,4,3,2,1], [10,9,8,7,6]],
-    ["/images/1", "/images/2", "/images/3"],
-    ["training", "training", "training"],
-    ['spoon', 'knife', 'fork']
+mutant_api.add(
+    embedding=[[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [10, 9, 8, 7, 6]],
+    input_uri=["/images/1", "/images/2", "/images/3"],
+    dataset="train",
+    inference_class=["spoon", "knife", "fork"],
+    model_space="sample_space"
 )
 
 print("count")
-print(mutant.count())
+print(mutant_api.count())
 
 print("Generating the index")
-print(mutant.create_index())
+print(mutant_api.create_index())
 
 print("Running a nearest neighbor search")
-print(mutant.get_nearest_neighbors([1, 2, 3, 4, 5], 1))
+print(mutant_api.get_nearest_neighbors([1, 2, 3, 4, 5], 1))
 
 print("Success Everything worked!")
