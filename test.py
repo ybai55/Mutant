c897eb168772a3fe39a615d429699dcbdfe34411
ybai55 <ybai24@hawk.iit.edu>
2024-04-08 15:53:23 +0800

    testing and building the api script

---
+++ test.py
@@ -0,0 +1,47 @@
+import mutantdb
+from mutantdb.config import Settings
+
+mutant = mutantdb.init(Settings(mutant_api_impl="rest",
+                                mutant_server_host="localhost",
+                                mutant_server_http_port="8000"))
+
+mutant.reset()
+
+print("heartbeat", mutant.heartbeat())
+
+print("create", mutant.create_collection("test", {"test", "test"}))
+
+
+# print("list", mutant.list_collections())
+
+# print("get", mutant.update_collection("test", {"test": "another"}))
+
+# print("get", mutant.get_collection("test"))
+
+# print("delete", mutant.delete_collection("test"))
+
+# print("list", mutant.list_collections())
+
+print("count", mutant.count("test"))
+
+print("add", mutant.add(
+    collection_name="test",
+    embedding=[[1.1, 2.3, 3.2], [4.5, 6.9, 4.4], [1.1, 2.3, 3.2], [4.5, 6.9, 4.4], [1.1, 2.3, 3.2], [4.5, 6.9, 4.4], [1.1, 2.3, 3.2], [4.5, 6.9, 4.4]], 
+))
+print("add", mutant.add(
+    collection_name="test",
+    embedding=[[5.1, 4.3, 3.2], [6.5, 5.9, 4.4]], 
+    metadata=[{"uri": "img11.png", "style": "style1"}, {"uri": "img10.png", "style": "style1"}]
+))
+print("add", mutant.add(
+    collection_name="test",
+    embedding=[[11.0, 12.0, 13.0]], 
+    metadata=[{"uri": "img12.png", "style": "style1"}]
+))
+# print("add", mutant.add(
+#     collection_name="test",
+#     embedding=[[1.1, 2.3, 3.2], [4.5, 6.9, 4.4]], 
+#     metadata=[{"apples": "bananas"}, {"apples": "oranges"}]
+# ))
+
+# mutant.set_collection_name("test")
